f3eca3bed9049541a9ed984c36e1e9e02b573473
Daniel Connolly <daniel@dconnolly.com>
2024-10-20 21:44:43 +0000

    add todo of error

--- cli/src/ba.rs
+++ cli/src/ba.rs
@@ -1,14 +1,14 @@
-use std::collections::{BTreeSet, VecDeque};
-use std::io::Cursor;
 use bitcoinsv::bitcoin::{BlockHash, FullBlockStream, ToHex};
 use bitcoinsv_rpc::{Auth, Client, GetChainTipsResultStatus, RpcApi};
-use tokio_stream::StreamExt;
-use url::Url;
 use bsvdb_base::BlockArchiveConfig;
 use bsvdb_blockarchive::{BlockArchive, Error, SimpleFileBasedBlockArchive};
+use std::collections::{BTreeSet, VecDeque};
+use std::io::Cursor;
+use tokio_stream::StreamExt;
+use url::Url;
 
 pub async fn list_blocks(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Result<()> {
-    let mut archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+    let mut archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     let mut results = archive.block_list().await.unwrap();
     while let Some(block_hash) = results.next().await {
         println!("{}", block_hash);
@@ -16,8 +16,9 @@ pub async fn list_blocks(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Res
     Ok(())
 }
 
+// todo: incorrectly reports genesis block as unlinked
 pub async fn check_links(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Result<()> {
-    let mut archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+    let mut archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     let mut block_it = archive.block_list().await.unwrap();
     // collect all hashes for checking parents
     let mut block_hashes = BTreeSet::new();
@@ -27,13 +28,13 @@ pub async fn check_links(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Res
     while let Some(block_hash) = block_it.next().await {
         block_hashes.insert(block_hash);
         let h = archive.block_header(&block_hash).await.unwrap();
-        if ! block_hashes.contains(&h.prev_hash) {
+        if !block_hashes.contains(&h.prev_hash) {
             not_found.push(h);
         }
     }
     // check the ones not found yet
     for h in not_found {
-        if ! block_hashes.contains(&h.prev_hash) {
+        if !block_hashes.contains(&h.prev_hash) {
             println!("dont have parent of block {}", h.hash())
         }
     }
@@ -79,8 +80,11 @@ pub async fn check_single_block(mut block: FullBlockStream) -> bsvdb_blockarchiv
 }
 
 // check the consistency of a single block
-pub async fn check_block(config: &BlockArchiveConfig, block_hash: BlockHash) -> bsvdb_blockarchive::Result<()> {
-    let archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+pub async fn check_block(
+    config: &BlockArchiveConfig,
+    block_hash: BlockHash,
+) -> bsvdb_blockarchive::Result<()> {
+    let archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     let reader = archive.get_block(&block_hash).await.unwrap();
     let block = FullBlockStream::new(reader).await.unwrap();
     println!("Block hash: {}", block.block_header.hash());
@@ -95,8 +99,11 @@ pub async fn check_block(config: &BlockArchiveConfig, block_hash: BlockHash) -> 
 }
 
 // check all blocks
-pub async fn check_all_blocks(config: &BlockArchiveConfig, verbose: bool) -> bsvdb_blockarchive::Result<()> {
-    let mut archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+pub async fn check_all_blocks(
+    config: &BlockArchiveConfig,
+    verbose: bool,
+) -> bsvdb_blockarchive::Result<()> {
+    let mut archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     let mut block_it = archive.block_list().await.unwrap();
     let mut num = 0;
     let mut errs = 0;
@@ -127,8 +134,12 @@ pub async fn check_all_blocks(config: &BlockArchiveConfig, verbose: bool) -> bsv
     Ok(())
 }
 
-pub async fn header(config: &BlockArchiveConfig, block_hash: BlockHash, hex: bool) -> bsvdb_blockarchive::Result<()> {
-    let archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+pub async fn header(
+    config: &BlockArchiveConfig,
+    block_hash: BlockHash,
+    hex: bool,
+) -> bsvdb_blockarchive::Result<()> {
+    let archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     match archive.block_header(&block_hash).await {
         Ok(h) => {
             if hex {
@@ -138,18 +149,14 @@ pub async fn header(config: &BlockArchiveConfig, block_hash: BlockHash, hex: boo
                 println!("{:?}", h);
             }
             Ok(())
-        },
-        Err(e) => {
-            match e {
-                Error::BlockNotFound => {
-                    println!("Block not found");
-                    Ok(())
-                },
-                _ => {
-                    Err(e)
-                }
-            }
         }
+        Err(e) => match e {
+            Error::BlockNotFound => {
+                println!("Block not found");
+                Ok(())
+            }
+            _ => Err(e),
+        },
     }
 }
 
@@ -157,7 +164,11 @@ pub async fn header(config: &BlockArchiveConfig, block_hash: BlockHash, hex: boo
 // for every chain tip:
 //      follow chain down until find a block we already have, putting each block on a stack
 //      follow chain back up, popping off stack, fetch the block and store it in block archive
-pub async fn rpc_import(config: &BlockArchiveConfig, rpc_uri: String, verbose: bool) -> bsvdb_blockarchive::Result<()> {
+pub async fn rpc_import(
+    config: &BlockArchiveConfig,
+    rpc_uri: String,
+    verbose: bool,
+) -> bsvdb_blockarchive::Result<()> {
     let uri;
     let username;
     let password;
@@ -167,45 +178,67 @@ pub async fn rpc_import(config: &BlockArchiveConfig, rpc_uri: String, verbose: b
             return Ok(());
         }
         Ok(url) => {
-            uri = format!("{}://{}:{}/", url.scheme(), url.host_str().unwrap(), url.port().unwrap());
+            uri = format!(
+                "{}://{}:{}/",
+                url.scheme(),
+                url.host_str().unwrap(),
+                url.port().unwrap()
+            );
             username = String::from(url.username());
             password = String::from(url.password().unwrap());
         }
     }
-    let archive= SimpleFileBasedBlockArchive::new(config).await.unwrap();
+    let archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
     let rpc_client = Client::new(&*uri, Auth::UserPass(username, password), None).unwrap();
     let chain_tips = rpc_client.get_chain_tips().unwrap();
     let num_tips = chain_tips.len();
-    let mut known_hashes = BTreeSet::new();     // set of hashes that are known and we either have it already or will get it
+    let mut known_hashes = BTreeSet::new(); // set of hashes that are known and we either have it already or will get it
     let mut fetched = 0;
     for t in chain_tips {
-        if verbose { println!("checking chain tip {}", t.hash);}
-        if t.status == GetChainTipsResultStatus::Active || t.status == GetChainTipsResultStatus::ValidFork
-            || t.status == GetChainTipsResultStatus::ValidHeaders {
+        if verbose {
+            println!("checking chain tip {}", t.hash);
+        }
+        if t.status == GetChainTipsResultStatus::Active
+            || t.status == GetChainTipsResultStatus::ValidFork
+            || t.status == GetChainTipsResultStatus::ValidHeaders
+        {
             // follow chain down
-            let mut fetch_hashes = Vec::new();              // stack of hashes of blocks to get
+            let mut fetch_hashes = Vec::new(); // stack of hashes of blocks to get
             let mut hash = t.hash;
-            while ! known_hashes.contains(&hash) {
+            while !known_hashes.contains(&hash) {
                 known_hashes.insert(hash.clone());
-                if ! archive.block_exists(&hash).await.unwrap() {
+                if !archive.block_exists(&hash).await.unwrap() {
                     fetch_hashes.push(hash.clone());
                     let h = rpc_client.get_block_header(&hash).unwrap();
                     hash = h.prev_hash;
                 }
             }
-            if verbose { println!("found known hash {}, need to fetch {} blocks", hash, fetch_hashes.len());}
+            if verbose {
+                println!(
+                    "found known hash {}, need to fetch {} blocks",
+                    hash,
+                    fetch_hashes.len()
+                );
+            }
             // fetch them
             while let Some(h) = fetch_hashes.pop() {
                 let mut fb = rpc_client.get_block_binary(&h).await.unwrap();
                 archive.store_block(&h, &mut fb).await.unwrap();
-                if verbose { println!("stored block {}", h); }
+                if verbose {
+                    println!("stored block {}", h);
+                }
                 fetched += 1
             }
         } else {
             // todo: this ignores the entire chain tip, there might be blocks down there that we should get
-            if verbose { println!("ignoring chain tip {}", t.hash);}
+            if verbose {
+                println!("ignoring chain tip {}", t.hash);
+            }
         }
     }
-    println!("checked {} chain tips, imported {} blocks ", num_tips, fetched);
+    println!(
+        "checked {} chain tips, imported {} blocks ",
+        num_tips, fetched
+    );
     Ok(())
 }
