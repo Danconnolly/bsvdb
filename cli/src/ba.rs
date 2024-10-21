ab93f636d247a72ca9c254c5e490efc3683ab6af
Daniel Connolly <daniel@dconnolly.com>
2024-10-21 17:05:59 +0000

    reorg ba check_links function

--- cli/src/ba.rs
+++ cli/src/ba.rs
@@ -16,31 +16,6 @@ pub async fn list_blocks(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Res
     Ok(())
 }
 
-// todo: incorrectly reports genesis block as unlinked
-pub async fn check_links(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Result<()> {
-    let mut archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
-    let mut block_it = archive.block_list().await.unwrap();
-    // collect all hashes for checking parents
-    let mut block_hashes = BTreeSet::new();
-    // headers where we didnt find the parent on the first pass
-    let mut not_found = Vec::new();
-    // for each block
-    while let Some(block_hash) = block_it.next().await {
-        block_hashes.insert(block_hash);
-        let h = archive.block_header(&block_hash).await.unwrap();
-        if !block_hashes.contains(&h.prev_hash) {
-            not_found.push(h);
-        }
-    }
-    // check the ones not found yet
-    for h in not_found {
-        if !block_hashes.contains(&h.prev_hash) {
-            println!("dont have parent of block {}", h.hash())
-        }
-    }
-    Ok(())
-}
-
 // check a single block, returns true if all ok, false otherwise
 pub async fn check_single_block(mut block: FullBlockStream) -> bsvdb_blockarchive::Result<bool> {
     // collect transaction hashes
---
+++ cli/src/chk_links.rs
@@ -0,0 +1,29 @@
+use bsvdb_base::BlockArchiveConfig;
+use bsvdb_blockarchive::{BlockArchive, SimpleFileBasedBlockArchive};
+use std::collections::BTreeSet;
+
+/// BlockArchive check links
+// todo: incorrectly reports genesis block as unlinked
+pub async fn check_links(config: &BlockArchiveConfig) -> bsvdb_blockarchive::Result<()> {
+    let mut archive = SimpleFileBasedBlockArchive::new(config).await.unwrap();
+    let mut block_it = archive.block_list().await.unwrap();
+    // collect all hashes for checking parents
+    let mut block_hashes = BTreeSet::new();
+    // headers where we didnt find the parent on the first pass
+    let mut not_found = Vec::new();
+    // for each block
+    while let Some(block_hash) = block_it.next().await {
+        block_hashes.insert(block_hash);
+        let h = archive.block_header(&block_hash).await.unwrap();
+        if !block_hashes.contains(&h.prev_hash) {
+            not_found.push(h);
+        }
+    }
+    // check the ones not found yet
+    for h in not_found {
+        if !block_hashes.contains(&h.prev_hash) {
+            println!("dont have parent of block {}", h.hash())
+        }
+    }
+    Ok(())
+}
